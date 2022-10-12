6b0876d3793131ae38e58e220bf05f1ee8b055c5
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2022-10-12 23:20:20 +0100

    feat(email_classificator): implement node

---
+++ classificator/src/email_classificator/main.py
@@ -0,0 +1,22 @@
+import random
+
+from proto.internal_nodes_pb2 import EtlOutput, EmailCategory, Email, CATEGORY_ADMINISTRATION, CATEGORY_REPARATIONS, \
+    CATEGORY_BILLING, CATEGORY_SPAM, ClassificatorOutput
+
+
+async def default_handler(ctx, req):
+    etl_output = EtlOutput()
+    req.Unpack(etl_output)
+    for email in etl_output.emails:
+        category = classify_email(email)
+        res = ClassificatorOutput()
+        res.email.CopyFrom(email)
+        res.category = category
+        if category == CATEGORY_REPARATIONS:
+            await ctx.send_output(res, "repairs")
+        await ctx.send_output(res)
+
+
+def classify_email(_: Email) -> EmailCategory:
+    categories = [CATEGORY_REPARATIONS, CATEGORY_ADMINISTRATION, CATEGORY_BILLING, CATEGORY_SPAM]
+    return categories[random.randint(0, len(categories) - 1)]
