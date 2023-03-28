3b9657f4b463a071b3a71c92a31f63f8565ac2da
Javier Perez <javier.perez@intelygenz.com>
2023-03-28 12:31:48 +0200

    Fix runner version for obj-store node, remove init from obj-store node, add new log to know payload size

--- classificator/src/obj_store/script_runner.py
+++ classificator/src/obj_store/script_runner.py
@@ -1,15 +1,11 @@
-import os
 from datetime import datetime
-
-def init(ctx):
-    ctx.logger.info("[executing init]")
-    ctx.logger.info(f"OBJ STORE ENV VAR: {os.environ.get('KRT_NATS_OBJECT_STORE')}")
-    ctx.logger.info(f"OBJECT STORE: {ctx.object_store}")
 
 async def default_handler(ctx, _):
     ctx.logger.info("[executing default handler]")
 
     payload = b'x' * int(1024 * 1024 * 1024 * 1.0)
+    ctx.logger.info(f"chosen payload has {len(payload)} bytes")
+
     now = datetime.now()
     await ctx.object_store.save("foo", payload)
     ctx.logger.info(f"put: {datetime.now() - now}")
