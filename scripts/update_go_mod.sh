be77b7c0af645a154cfae4cba448ba6f17e05369
David Fuentes <david.fuentes@intelygenz.com>
2023-05-04 11:20:40 +0200

    fix: update update_go_mod.sh with the new folder structure

--- scripts/update_go_mod.sh
+++ scripts/update_go_mod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Tidy repairs_handler node go.mod..."
-cd classificator/src/repairs_handler
+cd classificator/src/common/repairs_handler
 go mod tidy
 
 echo "Tidy stats_storer node go.mod...."
@@ -11,5 +11,14 @@ go mod tidy
 echo "Tidy exitpoint node go.mod..."
 cd ../exitpoint
 go mod tidy
+
+echo "Tidy email_classificator node go.mod..."
+cd ../../go-classificator/email_classificator
+go mod tidy
+
+echo "Tidy etl node go.mod..."
+cd ../etl
+go mod tidy
+
 
 echo "Done"
